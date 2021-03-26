009f95d9b8811c27862ff5bd7c9c4cfe81f37995
Rabiul hossain <mohammadrabiul4@gmail.com>
2021-03-26 10:04:01 -0700

    adding social-buddy

---
+++ src/components/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = (props) => {
+    const {id, title, body} = props.post;
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Post;
